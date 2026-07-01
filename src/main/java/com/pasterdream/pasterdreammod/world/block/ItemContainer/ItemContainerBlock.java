fe6a8f84f79e72a44e8100bab21121ecb03eee53
2884omgpy <1357834094@qq.com>
2026-07-01 15:32:22 +0800

    添加染梦教堂结构（0，2，4，6）

--- src/main/java/com/pasterdream/pasterdreammod/world/block/ItemContainer/ItemContainerBlock.java
+++ src/main/java/com/pasterdream/pasterdreammod/world/block/ItemContainer/ItemContainerBlock.java
@@ -12,9 +12,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -54,6 +52,18 @@ public abstract class ItemContainerBlock extends BaseEntityBlock
     public BlockState getStateForPlacement(BlockPlaceContext context)
     {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation)
+    {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror)
+    {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
     }
 
     @Override
